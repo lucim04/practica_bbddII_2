d8a664501e2cb903a1aee577c83c332b5d21b813
Laura García González <laura.gagonzalez@alumnos.upm.es>
2026-04-21 00:16:59 +0200

    Add files via upload

--- ejercicio1.py
+++ ejercicio1.py
@@ -14,23 +14,13 @@ Justificación:
 - Sink: escribe la salida en formato CSV en /files/ejercicio1 con ficheros radar-XXXXX.csv.
 '''
 
-
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.common.typeinfo import Types
-from pyflink.datastream.connectors.file_system import FileSink, OutputFileConfig
-from pyflink.common.serialization import Encoder
 import argparse
+from pyflink.common import Types
+from pyflink.common.serialization import Encoder
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors.file_system import FileSink, OutputFileConfig
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--input', required=True, help="Ruta del fichero de entrada")
-args = parser.parse_args()
-env = StreamExecutionEnvironment.get_execution_environment()
-env.set_parallelism(1)
-ds = env.read_text_file(args.input)
-
-
-def parse(line):
+def parse_line(line):
     fields = line.strip().split(",")
     return (
         int(fields[0]),  
@@ -43,33 +33,52 @@ def parse(line):
         int(fields[7])
     )
 
-parsed = ds.map(parse, output_type=Types.TUPLE([
-    Types.INT(), Types.INT(), Types.INT(), Types.INT(),
-    Types.INT(), Types.INT(), Types.INT(), Types.INT()
-])).filter(lambda x: x is not None)
+def to_output_csv(event):
+    return f"{event[0]},{event[1]},{event[3]},{event[2]}"
+
+def build_sink(output_path: str, prefix: str):
+    return (
+        FileSink
+        .for_row_format(output_path, Encoder.simple_string_encoder())
+        .with_output_file_config(
+            OutputFileConfig.builder()
+            .with_part_prefix(prefix)
+            .with_part_suffix(".csv")
+            .build()
+        )
+        .build()
+    )
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True, help="Ruta del fichero de entrada")
+    args = parser.parse_args()
+
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+
+    ds = env.read_text_file(args.input)
+
+    parsed = ds.map(
+        parse_line,
+        output_type=Types.TUPLE([
+            Types.INT(), Types.INT(), Types.INT(), Types.INT(),
+            Types.INT(), Types.INT(), Types.INT(), Types.INT()
+        ])
+    )
+
+    alerts = (
+        parsed
+        .filter(lambda x: x[2] > 90)
+        .map(to_output_csv, output_type=Types.STRING())
+    )
+
+    sink = build_sink("/files/ejercicio1", "radar")
+    alerts.sink_to(sink)
+
+    env.execute("Ejercicio 1 - Radar")
 
 
-speeding = parsed.filter(lambda x: x[2] > 90)
-result = speeding.map(
-    lambda x: f"{x[0]},{x[1]},{x[3]},{x[2]}",
-    output_type=Types.STRING()
-)
-
-
-sink = FileSink.for_row_format(
-    "/files/ejercicio1",
-    Encoder.simple_string_encoder()
-).with_output_file_config(
-    OutputFileConfig.builder()
-    .with_part_prefix("radar")
-    .with_part_suffix(".csv")
-    .build()
-).build()
-
-
-result.sink_to(sink)
-env.execute("Radar")
-
-
-
+if __name__ == "__main__":
+    main()
 
